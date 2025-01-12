753f648172e7d57fc2ee5852c4ef25dc91e60081
k.osawa <saataysu4408999@gmail.com>
2025-01-12 17:22:50 +0900

    modified html02

--- projects/template-simu01/src/app/app.component.ts
+++ projects/template-simu01/src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './html01/app.component01.html',
-  styleUrl: './html01/app.component01.scss'
+  templateUrl: './html02/app.component01.html',
+  styleUrl: './html02/app.component01.scss'
 })
 export class AppComponent {
   title = 'template-simu01';
