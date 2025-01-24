936af39699ad65e18e8ee09093357d4884f5aa56
k.osawa <saataysu4408999@gmail.com>
2025-01-24 20:07:53 +0900

    added html05

--- projects/template-simu01/src/app/app.component.ts
+++ projects/template-simu01/src/app/app.component.ts
@@ -1,13 +1,60 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {ChangeDetectorRef, NgZone, inject} from '@angular/core';
+import {A11yModule, FocusOrigin} from '@angular/cdk/a11y';
+
+import {
+  AfterViewInit,
+  ElementRef, 
+  OnDestroy,
+  ViewChild,  
+} from '@angular/core';
+import {FocusMonitor} from '@angular/cdk/a11y';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet ],
-  templateUrl: './html03/app.component.html',
-  styleUrl: './html03/app.component.scss'
+  imports: [ RouterOutlet, MatSlideToggleModule, MatFormFieldModule, MatSelectModule, A11yModule],
+  templateUrl: './html05/app.component.html',
+  styleUrl: './html05/app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, AfterViewInit {
   title = 'template-simu01';
+
+  private _ngZone = inject(NgZone);
+  private _cdr = inject(ChangeDetectorRef);
+  focusMonitor = inject(FocusMonitor);
+  
+  elementOrigin = this.formatOrigin(null);
+  subtreeOrigin = this.formatOrigin(null);
+
+  @ViewChild('monitored')
+  monitoredEl!: ElementRef<HTMLElement>;
+
+  origin = this.formatOrigin(null);
+
+  formatOrigin(origin: FocusOrigin): string {
+    return origin ? origin + ' focused' : 'blurred';
+  }
+
+  markForCheck() {
+    this._ngZone.run(() => this._cdr.markForCheck());
+  }
+
+  ngAfterViewInit() {
+    this.focusMonitor.monitor(this.monitoredEl).subscribe(origin =>
+      this._ngZone.run(() => {
+        this.origin = this.formatOrigin(origin);
+        this._cdr.markForCheck();
+      }),
+    );
+  }
+
+  ngOnDestroy() {
+    this.focusMonitor.stopMonitoring(this.monitoredEl);
+  }
+
   
 }
--- projects/template-simu01/src/app/app.config.ts
+++ projects/template-simu01/src/app/app.config.ts
@@ -4,11 +4,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 //import { provideImgixLoader } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
+    provideRouter(routes), provideAnimationsAsync(),
     //provideAnimations(),
     //provideHttpClient()
     //provideImgixLoader('')<===CDNの画像で必要
