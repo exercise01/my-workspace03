3c2594ae195863dad9fa12c76f06a7595a6d5295
k.osawa <saataysu4408999@gmail.com>
2025-01-30 16:24:48 +0900

    modified template-simu02

--- projects/template-simu02/src/app/app.component.ts
+++ projects/template-simu02/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {A11yModule, FocusOrigin} from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './html01/app.component.html',
-  styleUrl: './html01/app.component.scss'
+  imports: [RouterOutlet, A11yModule],
+  templateUrl: './html02/app.component.html',
+  styleUrl: './html02/app.component.scss'
 })
 export class AppComponent {
   title = 'template-simu02';
