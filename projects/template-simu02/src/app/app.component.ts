3b3bb9aaa3514f9167ca3fc31f11e7b299a0b565
k.osawa <saataysu4408999@gmail.com>
2025-02-02 17:10:42 +0900

    added html04

--- projects/template-simu02/src/app/app.component.ts
+++ projects/template-simu02/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {A11yModule, FocusOrigin} from '@angular/cdk/a11y';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, A11yModule],
-  templateUrl: './html02/app.component.html',
-  styleUrl: './html02/app.component.scss'
+  imports: [RouterOutlet, A11yModule, MatButtonModule, MatCardModule],
+  templateUrl: './html04/app.component.html',
+  styleUrl: './html04/app.component.scss'
 })
 export class AppComponent {
   title = 'template-simu02';
