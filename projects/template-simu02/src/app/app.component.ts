668e9860ae1caa0aa0938a8af0b9642554c7acd9
k.osawa <saataysu4408999@gmail.com>
2025-02-03 08:40:45 +0900

    added html05 item list-item

--- projects/template-simu02/src/app/app.component.ts
+++ projects/template-simu02/src/app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {A11yModule, FocusOrigin} from '@angular/cdk/a11y';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { A11yModule, FocusOrigin } from '@angular/cdk/a11y';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ListComponent } from './item/item.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, A11yModule, MatButtonModule, MatCardModule],
-  templateUrl: './html04/app.component.html',
-  styleUrl: './html04/app.component.scss'
+  imports: [RouterOutlet, A11yModule,
+    MatButtonModule, MatCardModule, 
+    ListComponent, ListItemComponent],
+  templateUrl: './html05/app.component.html',
+  styleUrl: './html05/app.component.scss',
 })
 export class AppComponent {
   title = 'template-simu02';
+
+  itemLabel = '選択してください';
+
+  fruits = [
+    'Apples',
+    'Bananas',
+    'Cherries',
+    'Dewberries',
+    'Blueberries',
+    'Avocados',
+  ];
+
+  isVisibleListItem = false;
+
+  onClickButton(): void {
+    this.isVisibleListItem = !this.isVisibleListItem;
+  }
 }
